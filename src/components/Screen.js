ee4e04aa60ac108042c13d10c3373f14955cfe14
Lex Rabkin <rabkin.alexander@gmail.com>
2021-10-07 15:33:47 -0400

    Calculator layout styles and propTypes

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,18 +1,31 @@
-import { Component } from 'react';
-import styled from 'styled-components';
+import { Component } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Container = styled.div`
   border: 1px solid black;
-  width: 300px;
+  width: 410px;
   height: 70px;
   text-align: right;
   margin-bottom: 10px;
+  margin-left: 14px;
+  padding: 5px;
 `;
 
 class Screen extends Component {
   render() {
-    return <Container>{this.props.value}</Container>;
+    return (
+      <Container>
+        <div>{this.props.value.first}</div>
+        <div>{this.props.value.operator}</div>
+        <div>{this.props.value.second}</div>
+      </Container>
+    );
   }
 }
+
+Screen.propTypes = {
+  value: PropTypes.object,
+};
 
 export default Screen;
